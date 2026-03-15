846a767131d81d72f12815dfd3eaa7caf165f12d
Hridrita <moonsaha721@gmail.com>
2026-03-15 16:40:47 +0600

    created a  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Countries from './components/countries'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>React World on the Go</h1>
+    <Countries></Countries>
+      
     </>
   )
 }
