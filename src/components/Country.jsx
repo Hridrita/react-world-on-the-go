b438a24972c592aa434984d219010587a66510fb
Hridrita <moonsaha721@gmail.com>
2026-03-15 17:46:53 +0600

    adding flag border color

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
 
 const Country = ({country}) => {
-    console.log(country.name.common);
+    const [visited, setVisited] = useState(false);
+
+   // console.log(country.name.common);
 
     const handleVisited = () =>{
-        console.log('button clicked');
+        //console.log('button clicked');
+
+
+       if(visited){
+        setVisited(false)
+       }
+       else{
+        setVisited(true)
+       }
     }
+
+
+    //  setVisited(visited ? false : true)
+
+
     return (
-        <div className='country'>
+        <div className={`country`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h3>Name: {country.name.common} </h3>
             <p>Population: {country.population.population}</p>
             <p>Area:{country.area.area}
                  {country.area.area > 300000 ?'Big Country' : 'Small Country'}</p>
 
-                 <button onClick={handleVisited}>Not Visited</button>
+                 <button onClick={handleVisited}>
+                    {
+                        visited ? 'Visited' : 'Not Visited'
+                    }
+                 </button>
             
         </div>
     );
